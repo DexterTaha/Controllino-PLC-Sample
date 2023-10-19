5bf742769693713d5f516341f0ad47990d4805b0
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-19 14:22:14 +0100

    Update TC100_AND_OR_NOT.ino

--- TC100/TC100_AND_OR_NOT/TC100_AND_OR_NOT.ino
+++ TC100/TC100_AND_OR_NOT/TC100_AND_OR_NOT.ino
@@ -30,18 +30,37 @@ pinMode(CONTROLLINO_D3, OUTPUT);
 void loop() {
 
 // AND
+
+// 0 0    0
+// 0 1    0
+// 1 0    0
+// 1 1    1
+    
 if (digitalRead (CONTROLLINO_A0 ) && digitalRead (CONTROLLINO_A1 ))
     digitalWrite (CONTROLLINO_D0 , true);
     else
     digitalWrite (CONTROLLINO_D0 , false);
 
 // OR
+    
+
+// 0 0    0
+// 0 1    1
+// 1 0    1
+// 1 1    1
+    
 if (digitalRead (CONTROLLINO_A3 ) || digitalRead (CONTROLLINO_A4 ))
     digitalWrite (CONTROLLINO_D1 , true);
     else
     digitalWrite (CONTROLLINO_D1 , false);
 
 // NOT
+    
+
+// 0    1
+// 1    0
+
+    
 if (digitalRead (CONTROLLINO_A6 ) == true )
     digitalWrite (CONTROLLINO_D2 , false);
     else
