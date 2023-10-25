92778ec6829b3c6d3254faa39c7ce8e0bc878a77
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-25 10:45:32 +0100

    Update TC208_Analog_value.ino

--- TC200/TC208_Analog_value/TC208_Analog_value.ino
+++ TC200/TC208_Analog_value/TC208_Analog_value.ino
@@ -2,24 +2,23 @@
 #include <Controllino.h>
 
 //
-//  Task: TC208_Analog_value
+//  Task: TC209_Fast_Counter
 //
 
 //  definition of variables
 
-int   iValue;            // analog value
-                         // 0 - 1024  equals to 0V - 24V
-                         // Analog power on the Trainingsboard is 0V - 10V
-                         // usable values
-                         //   100% -> 320
-                         //   and so on
+int   iCount;                      // Count pulse per second
+int   iLastCount=3;                // Count from last second
+unsigned long previousMillis = 0;  // stored milliseconds
+unsigned long currentMillis;       // actual milliseconds
+
 
 //
 //  Setup
 //
 void setup() {
 
-pinMode(CONTROLLINO_A6, INPUT);
+pinMode(CONTROLLINO_A0, INPUT);
 
 pinMode(CONTROLLINO_D0, OUTPUT);
 pinMode(CONTROLLINO_D1, OUTPUT);
@@ -30,6 +29,14 @@ pinMode(CONTROLLINO_D5, OUTPUT);
 pinMode(CONTROLLINO_D6, OUTPUT);
 pinMode(CONTROLLINO_D7, OUTPUT);
 
+// Interrupt handler
+pinMode(CONTROLLINO_IN0, INPUT);
+attachInterrupt(digitalPinToInterrupt(CONTROLLINO_IN0), count, RISING );
+// LOW      to trigger the interrupt whenever the pin is low,
+// CHANGE   to trigger the interrupt whenever the pin changes value
+// RISING   to trigger when the pin goes from low to high,
+// FALLING  for when the pin goes from high to low.
+
 }
 
 
@@ -38,55 +45,70 @@ pinMode(CONTROLLINO_D7, OUTPUT);
 //
 void loop() {
 
-// Read analog value
-iValue = analogRead ( CONTROLLINO_A6 );
+// current time
+currentMillis = millis();
 
-// iValue > 32
-if ( (iValue > 32) )
+// assign iLastCount = iCount  
+if ( currentMillis - previousMillis >= 1000 ) 
+  {
+    // each edge change triggers an interrupt
+    // therefore each pulse is countes twice
+    iLastCount = iCount;
+    iCount = 0;
+    previousMillis = currentMillis;
+  }
+
+// iLastCount > 100
+if ( (iLastCount >= 100) )
     digitalWrite ( CONTROLLINO_D0, HIGH);
   else
     digitalWrite ( CONTROLLINO_D0, LOW);
 
-// iValue > 64
-if ( iValue > 64 )
+// iLastCount > 200
+if ( iLastCount > 200 )
     digitalWrite ( CONTROLLINO_D1, HIGH);
   else
     digitalWrite ( CONTROLLINO_D1, LOW);
 
-// iValue > 96
-if ( iValue > 96 )
+// iLastCount > 300
+if ( iLastCount > 300 )
     digitalWrite ( CONTROLLINO_D2, HIGH);
   else
     digitalWrite ( CONTROLLINO_D2, LOW);
 
-// iValue > 128
-if ( iValue > 128)
+// iLastCount > 400
+if ( iLastCount > 400 )
     digitalWrite ( CONTROLLINO_D3, HIGH);
   else
     digitalWrite ( CONTROLLINO_D3, LOW);
 
-// iValue > 160
-if ( iValue > 160 )
+// iLastCount > 500
+if ( iLastCount > 500 )
     digitalWrite ( CONTROLLINO_D4, HIGH);
   else
     digitalWrite ( CONTROLLINO_D4, LOW);
 
-// iValue > 192
-if ( iValue > 192 )
+// iLastCount > 600
+if ( iLastCount > 600 )
     digitalWrite ( CONTROLLINO_D5, HIGH);
   else
     digitalWrite ( CONTROLLINO_D5, LOW);
 
-// iValue > 224
-if ( iValue > 224 )
+// iLastCount > 700
+if ( iLastCount > 700 )
     digitalWrite ( CONTROLLINO_D6, HIGH);
   else
     digitalWrite ( CONTROLLINO_D6, LOW);
 
-// iValue > 256
-if ( iValue > 256 )
+// iLastCount > 800
+if ( iLastCount > 800 )
     digitalWrite ( CONTROLLINO_D7, HIGH);
   else
     digitalWrite ( CONTROLLINO_D7, LOW);
 
 } //loop
+
+
+void count() {
+ iCount++;
+}
