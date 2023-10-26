a77388f0ac7fc3ad1aaae97afd08c0b62c999d76
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-26 11:30:11 +0100

    Create TC309_Heating_house.ino

---
+++ TC300/TC309_Heating_house/TC309_Heating_house.ino
@@ -0,0 +1,108 @@
+#include <SPI.h>
+#include <Controllino.h>
+
+//
+//  Task: TC310_Awning_control
+//
+
+//  definition of variables
+
+bool bOn;             // Automatic mode-true, manuell mode-false
+bool bThermCutOff;    // switch overtemperature
+int  iTempBoiler;     // actual temperature boler
+int  iTempRoom;       // actual temperature room
+
+//
+//  Setup
+//
+void setup() {
+
+pinMode(CONTROLLINO_A0, INPUT);
+pinMode(CONTROLLINO_A1, INPUT);
+pinMode(CONTROLLINO_A2, INPUT);
+pinMode(CONTROLLINO_A3, INPUT);
+pinMode(CONTROLLINO_A4, INPUT);
+pinMode(CONTROLLINO_A5, INPUT);
+pinMode(CONTROLLINO_A6, INPUT);
+pinMode(CONTROLLINO_A7, INPUT);
+
+pinMode(CONTROLLINO_D0, OUTPUT);
+pinMode(CONTROLLINO_D1, OUTPUT);
+pinMode(CONTROLLINO_D2, OUTPUT);
+pinMode(CONTROLLINO_D3, OUTPUT);
+pinMode(CONTROLLINO_D4, OUTPUT);
+pinMode(CONTROLLINO_D5, OUTPUT);
+pinMode(CONTROLLINO_D6, OUTPUT);
+pinMode(CONTROLLINO_D7, OUTPUT);
+
+}
+
+
+//
+//  Loop
+//
+void loop() {
+
+// Read analog values from wind- and lightsensor
+iTempRoom = analogRead (CONTROLLINO_A6);
+iTempBoiler = analogRead (CONTROLLINO_A7);
+
+// detect on / off
+if ( digitalRead (CONTROLLINO_A0) )
+    bOn = true;
+  else
+    bOn = false;
+
+// detect RESET termal cut-off boiler
+if ( digitalRead (CONTROLLINO_A1) ) 
+  {
+    bThermCutOff = false;
+  }
+
+// detect termalcut-off boiler
+if ( !digitalRead (CONTROLLINO_A3) ) 
+  {
+    bThermCutOff = true;
+  }
+
+// Heating torch on/off
+if ( (iTempBoiler<200) && !bThermCutOff && bOn)
+  {
+    digitalWrite (CONTROLLINO_D1, HIGH);
+  }
+  else
+  {
+    digitalWrite (CONTROLLINO_D1, LOW);
+  }
+
+// Pump for room heating on/off
+if ( (iTempRoom<100) && !bThermCutOff && bOn)
+  {
+    digitalWrite (CONTROLLINO_D3, HIGH);
+  }
+  else
+  {
+    digitalWrite (CONTROLLINO_D3, LOW);
+  }
+
+// Signaling Heating on
+if ( bOn )
+  {
+    digitalWrite (CONTROLLINO_D0, HIGH);
+  }
+  else
+  {
+    digitalWrite (CONTROLLINO_D0, LOW);
+  }
+
+// Signaling ALARM
+if ( bThermCutOff )
+  {
+    digitalWrite (CONTROLLINO_D2, HIGH);
+  }
+  else
+  {
+    digitalWrite (CONTROLLINO_D2, LOW);
+  }
+  
+} //loop
