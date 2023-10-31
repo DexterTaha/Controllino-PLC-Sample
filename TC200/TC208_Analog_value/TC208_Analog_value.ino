6772e4a5df4c779ed718528be8c56b1f7456d35e
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:26:19 +0100

    Update TC208_Analog_value.ino

--- TC200/TC208_Analog_value/TC208_Analog_value.ino
+++ TC200/TC208_Analog_value/TC208_Analog_value.ino
@@ -2,113 +2,112 @@
 #include <Controllino.h>
 
 //
-//  Task: TC209_Fast_Counter
+// Tâche : TC209_Compteur_Rapide
 //
 
-//  definition of variables
+// Définition des variables
 
-int   iCount;                      // Count pulse per second
-int   iLastCount=3;                // Count from last second
-unsigned long previousMillis = 0;  // stored milliseconds
-unsigned long currentMillis;       // actual milliseconds
+int   iCompteur;                  // Compter les impulsions par seconde
+int   iDernierCompteur = 3;       // Compter depuis la dernière seconde
+unsigned long tempsPrecedent = 0;  // Millisecondes précédentes stockées
+unsigned long tempsActuel;         // Millisecondes actuelles
 
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-pinMode(CONTROLLINO_D7, OUTPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
+  pinMode(CONTROLLINO_D7, OUTPUT);
 
-// Interrupt handler
-pinMode(CONTROLLINO_IN0, INPUT);
-attachInterrupt(digitalPinToInterrupt(CONTROLLINO_IN0), count, RISING );
-// LOW      to trigger the interrupt whenever the pin is low,
-// CHANGE   to trigger the interrupt whenever the pin changes value
-// RISING   to trigger when the pin goes from low to high,
-// FALLING  for when the pin goes from high to low.
+  // Gestionnaire d'interruption
+  pinMode(CONTROLLINO_IN0, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CONTROLLINO_IN0), compter, RISING);
+  // LOW pour déclencher l'interruption chaque fois que la broche est basse,
+  // CHANGE pour déclencher l'interruption chaque fois que la broche change de valeur
+  // RISING pour déclencher lorsque la broche passe de basse à haute,
+  // FALLING pour déclencher lorsque la broche passe de haute à basse.
 
 }
 
 
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// current time
-currentMillis = millis();
+  // Temps actuel
+  tempsActuel = millis();
 
-// assign iLastCount = iCount  
-if ( currentMillis - previousMillis >= 1000 ) 
+  // attribuer iDernierCompteur = iCompteur
+  if (tempsActuel - tempsPrecedent >= 1000)
   {
-    // each edge change triggers an interrupt
-    // therefore each pulse is countes twice
-    iLastCount = iCount;
-    iCount = 0;
-    previousMillis = currentMillis;
+    // Chaque changement de front déclenche une interruption,
+    // donc chaque impulsion est comptée deux fois
+    iDernierCompteur = iCompteur;
+    iCompteur = 0;
+    tempsPrecedent = tempsActuel;
   }
 
-// iLastCount > 100
-if ( (iLastCount >= 100) )
-    digitalWrite ( CONTROLLINO_D0, HIGH);
+  // iDernierCompteur > 100
+  if (iDernierCompteur >= 100)
+    digitalWrite(CONTROLLINO_D0, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D0, LOW);
+    digitalWrite(CONTROLLINO_D0, LOW);
 
-// iLastCount > 200
-if ( iLastCount > 200 )
-    digitalWrite ( CONTROLLINO_D1, HIGH);
+  // iDernierCompteur > 200
+  if (iDernierCompteur > 200)
+    digitalWrite(CONTROLLINO_D1, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D1, LOW);
+    digitalWrite(CONTROLLINO_D1, LOW);
 
-// iLastCount > 300
-if ( iLastCount > 300 )
-    digitalWrite ( CONTROLLINO_D2, HIGH);
+  // iDernierCompteur > 300
+  if (iDernierCompteur > 300)
+    digitalWrite(CONTROLLINO_D2, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D2, LOW);
+    digitalWrite(CONTROLLINO_D2, LOW);
 
-// iLastCount > 400
-if ( iLastCount > 400 )
-    digitalWrite ( CONTROLLINO_D3, HIGH);
+  // iDernierCompteur > 400
+  if (iDernierCompteur > 400)
+    digitalWrite(CONTROLLINO_D3, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D3, LOW);
+    digitalWrite(CONTROLLINO_D3, LOW);
 
-// iLastCount > 500
-if ( iLastCount > 500 )
-    digitalWrite ( CONTROLLINO_D4, HIGH);
+  // iDernierCompteur > 500
+  if (iDernierCompteur > 500)
+    digitalWrite(CONTROLLINO_D4, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D4, LOW);
+    digitalWrite(CONTROLLINO_D4, LOW);
 
-// iLastCount > 600
-if ( iLastCount > 600 )
-    digitalWrite ( CONTROLLINO_D5, HIGH);
+  // iDernierCompteur > 600
+  if (iDernierCompteur > 600)
+    digitalWrite(CONTROLLINO_D5, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D5, LOW);
+    digitalWrite(CONTROLLINO_D5, LOW);
 
-// iLastCount > 700
-if ( iLastCount > 700 )
-    digitalWrite ( CONTROLLINO_D6, HIGH);
+  // iDernierCompteur > 700
+  if (iDernierCompteur > 700)
+    digitalWrite(CONTROLLINO_D6, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D6, LOW);
+    digitalWrite(CONTROLLINO_D6, LOW);
 
-// iLastCount > 800
-if ( iLastCount > 800 )
-    digitalWrite ( CONTROLLINO_D7, HIGH);
+  // iDernierCompteur > 800
+  if (iDernierCompteur > 800)
+    digitalWrite(CONTROLLINO_D7, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D7, LOW);
+    digitalWrite(CONTROLLINO_D7, LOW);
 
 } //loop
 
-
-void count() {
- iCount++;
+void compter() {
+  iCompteur++;
 }
