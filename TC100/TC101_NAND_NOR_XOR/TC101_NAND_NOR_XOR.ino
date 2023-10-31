378281c6b1912fb72ce00a34ba83836ea8efaed6
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 10:46:04 +0100

    Update TC101_NAND_NOR_XOR.ino

--- TC100/TC101_NAND_NOR_XOR/TC101_NAND_NOR_XOR.ino
+++ TC100/TC101_NAND_NOR_XOR/TC101_NAND_NOR_XOR.ino
@@ -2,66 +2,58 @@
 #include <Controllino.h>
 
 //
-//  Task: TC101_NAND_NOR_XOR
+//  Tâche : TC101_NAND_NOR_XOR
 //
 
 //
-//  Setup
+//  Configuration
 //
 void setup() {
-
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A3, INPUT);
-pinMode(CONTROLLINO_A4, INPUT);
-pinMode(CONTROLLINO_A5, INPUT);
-pinMode(CONTROLLINO_A6, INPUT);
-
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-
+  // Définir les broches en tant qu'entrées ou sorties
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A3, INPUT);
+  pinMode(CONTROLLINO_A4, INPUT);
+  pinMode(CONTROLLINO_A5, INPUT);
+  pinMode(CONTROLLINO_A6, INPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
 }
 
-
 //
-//  Loop
+//  Boucle
 //
 void loop() {
+  // Portes logiques NAND, NOR et XOR
 
-// NAND
+  // NAND
+  // 0 0    1
+  // 0 1    1
+  // 1 0    1
+  // 1 1    0
+  if (digitalRead(CONTROLLINO_A0) && digitalRead(CONTROLLINO_A1))
+    digitalWrite(CONTROLLINO_D0, LOW);
+  else
+    digitalWrite(CONTROLLINO_D0, HIGH);
 
-// 0 0    1
-// 0 1    1
-// 1 0    1
-// 1 1    0
+  // NOR
+  // 0 0    1
+  // 0 1    0
+  // 1 0    0
+  // 1 1    0
+  if (!digitalRead(CONTROLLINO_A3) && !digitalRead(CONTROLLINO_A4))
+    digitalWrite(CONTROLLINO_D1, HIGH);
+  else
+    digitalWrite(CONTROLLINO_D1, LOW);
 
-if (digitalRead (CONTROLLINO_A0 ) && digitalRead (CONTROLLINO_A1 ))
-    digitalWrite (CONTROLLINO_D0 , false);
-    else
-    digitalWrite (CONTROLLINO_D0 , true);
-
-// NOR
-
-// 0 0    1
-// 0 1    0
-// 1 0    0
-// 1 1    0
-
-if (!digitalRead (CONTROLLINO_A3 ) && !digitalRead (CONTROLLINO_A4 ))
-    digitalWrite (CONTROLLINO_D1 , true);
-    else
-    digitalWrite (CONTROLLINO_D1 , false);
-
-// XOR
-
-// 0 0    0
-// 0 1    1
-// 1 0    1
-// 1 1    0
-
-if ((digitalRead (CONTROLLINO_A5 ) && !digitalRead (CONTROLLINO_A6 )) || (!digitalRead (CONTROLLINO_A5 ) && digitalRead (CONTROLLINO_A6 )))
-    digitalWrite (CONTROLLINO_D2 , true);
-    else
-    digitalWrite (CONTROLLINO_D2 , false);
-} 
+  // XOR
+  // 0 0    0
+  // 0 1    1
+  // 1 0    1
+  // 1 1    0
+  if ((digitalRead(CONTROLLINO_A5) && !digitalRead(CONTROLLINO_A6)) || (!digitalRead(CONTROLLINO_A5) && digitalRead(CONTROLLINO_A6)))
+    digitalWrite(CONTROLLINO_D2, HIGH);
+  else
+    digitalWrite(CONTROLLINO_D2, LOW);
+}
