7431f218394379ffa045f3bdd5126061b5d513fe
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:33:10 +0100

    Update TC302_Counter.ino

--- TC300/TC302_Counter/TC302_Counter.ino
+++ TC300/TC302_Counter/TC302_Counter.ino
@@ -2,123 +2,120 @@
 #include <Controllino.h>
 
 //
-//  Task: TC302_Counter
+// Tâche : TC302_Compteur
 //
 
-//  definition of variables
+// Définition des variables
 
-int   iCounter=0;    // counter from 0 to 7
-bool  bOn;           // true-On, false-Off
-bool  bFlank=LOW;   // debounce light barrier 
+int iCompteur = 0;  // compteur de 0 à 7
+bool bAllume;      // true-Allumé, false-Éteint
+bool bFlanc = LOW;  // debounce pour la barrière de lumière
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A2, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A2, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-pinMode(CONTROLLINO_D7, OUTPUT);
-
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
+  pinMode(CONTROLLINO_D7, OUTPUT);
 }
 
-
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// Check if counter is over 7
-if (iCounter == 8)
-  iCounter=7;
+  // Vérifier si le compteur dépasse 7
+  if (iCompteur == 8)
+    iCompteur = 7;
 
-// Check if counter is under 0
-if (iCounter == -1)
-  iCounter=0;
+  // Vérifier si le compteur est inférieur à 0
+  if (iCompteur == -1)
+    iCompteur = 0;
 
-// iCounter = 0
-if ( iCounter == 0)
-    digitalWrite ( CONTROLLINO_D0, HIGH);
+  // iCompteur = 0
+  if (iCompteur == 0)
+    digitalWrite(CONTROLLINO_D0, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D0, LOW);
+    digitalWrite(CONTROLLINO_D0, LOW);
 
-// iCounter = 1
-if ( iCounter == 1) 
-    digitalWrite ( CONTROLLINO_D1, HIGH);
+  // iCompteur = 1
+  if (iCompteur == 1)
+    digitalWrite(CONTROLLINO_D1, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D1, LOW);
+    digitalWrite(CONTROLLINO_D1, LOW);
 
-// iCounter = 2
-if (iCounter == 2) 
-    digitalWrite ( CONTROLLINO_D2, HIGH);
+  // iCompteur = 2
+  if (iCompteur == 2)
+    digitalWrite(CONTROLLINO_D2, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D2, LOW);
+    digitalWrite(CONTROLLINO_D2, LOW);
 
-// iCounter = 3
-if (iCounter == 3) 
-    digitalWrite ( CONTROLLINO_D3, HIGH);
+  // iCompteur = 3
+  if (iCompteur == 3)
+    digitalWrite(CONTROLLINO_D3, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D3, LOW);
+    digitalWrite(CONTROLLINO_D3, LOW);
 
-// iCounter = 4
-if (iCounter == 4) 
-    digitalWrite ( CONTROLLINO_D4, HIGH);
+  // iCompteur = 4
+  if (iCompteur == 4)
+    digitalWrite(CONTROLLINO_D4, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D4, LOW);
+    digitalWrite(CONTROLLINO_D4, LOW);
 
-// iCounter = 5
-if (iCounter == 5) 
-    digitalWrite ( CONTROLLINO_D5, HIGH);
+  // iCompteur = 5
+  if (iCompteur == 5)
+    digitalWrite(CONTROLLINO_D5, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D5, LOW);
+    digitalWrite(CONTROLLINO_D5, LOW);
 
-// iCounter = 6
-if (iCounter == 6) 
-    digitalWrite ( CONTROLLINO_D6, HIGH);
+  // iCompteur = 6
+  if (iCompteur == 6)
+    digitalWrite(CONTROLLINO_D6, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D6, LOW);
+    digitalWrite(CONTROLLINO_D6, LOW);
 
-// iCounter = 7
-if (iCounter == 7) 
-    digitalWrite ( CONTROLLINO_D7, HIGH);
+  // iCompteur = 7
+  if (iCompteur == 7)
+    digitalWrite(CONTROLLINO_D7, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D7, LOW);
+    digitalWrite(CONTROLLINO_D7, LOW);
 
-// adjust counter +1
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A0) == HIGH ) )  
+  // Ajuster le compteur +1
+  if (bFlanc == LOW && digitalRead(CONTROLLINO_A0) == HIGH)
   {
-    iCounter++;
-    bFlank = true;
+    iCompteur++;
+    bFlanc = true;
   }
 
-// adjust counter -1
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A1) == HIGH ) )  
+  // Ajuster le compteur -1
+  if (bFlanc == LOW && digitalRead(CONTROLLINO_A1) == HIGH)
   {
-    iCounter--;
-    bFlank = true;
+    iCompteur--;
+    bFlanc = true;
   }
 
-// set counter ot 0
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A2) == HIGH ) )  
+  // Définir le compteur à 0
+  if (bFlanc == LOW && digitalRead(CONTROLLINO_A2) == HIGH)
   {
-    iCounter=0;
-    bFlank = true;
+    iCompteur = 0;
+    bFlanc = true;
   }
 
-// reset iFlanke bei Flankenwechsel High -> Low
-if ( !(digitalRead (CONTROLLINO_A0)) && !(digitalRead (CONTROLLINO_A1)) && !(digitalRead (CONTROLLINO_A2)) )  
+  // Réinitialiser bFlanc lors du changement de flanc de High à Low
+  if (!digitalRead(CONTROLLINO_A0) && !digitalRead(CONTROLLINO_A1) && !digitalRead(CONTROLLINO_A2))
   {
-    bFlank = false;
-    delay (20);     //
+    bFlanc = false;
+    delay(20); // Débordement
   }
-
-} //loop
+}
