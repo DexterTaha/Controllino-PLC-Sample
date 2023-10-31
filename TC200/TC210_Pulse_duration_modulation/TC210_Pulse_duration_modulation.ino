8ab850f6a33f501ccb0c177fea30114c79d82a1a
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:28:34 +0100

    Update TC210_Pulse_duration_modulation.ino

--- TC200/TC210_Pulse_duration_modulation/TC210_Pulse_duration_modulation.ino
+++ TC200/TC210_Pulse_duration_modulation/TC210_Pulse_duration_modulation.ino
@@ -2,54 +2,54 @@
 #include <Controllino.h>
 
 //
-//  Task: TC210_Pulse_duration_modulation
+// Tâche : TC210_Modulation_de_Durée_d'Impulsion
 //
 
-//  definition of variables
+// Définition des variables
 
-int   iTimeHigh;    // analog value for time high TH
-int   iTimeLow;     // analog value for time low  TL
+int iDureeHaute;  // Valeur analogique pour la durée haute TH
+int iDureeBasse;  // Valeur analogique pour la durée basse TL
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A2, INPUT);
-pinMode(CONTROLLINO_A3, INPUT);
-pinMode(CONTROLLINO_A4, INPUT);
-pinMode(CONTROLLINO_A5, INPUT);
-pinMode(CONTROLLINO_A6, INPUT);
-pinMode(CONTROLLINO_A7, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A2, INPUT);
+  pinMode(CONTROLLINO_A3, INPUT);
+  pinMode(CONTROLLINO_A4, INPUT);
+  pinMode(CONTROLLINO_A5, INPUT);
+  pinMode(CONTROLLINO_A6, INPUT);
+  pinMode(CONTROLLINO_A7, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-pinMode(CONTROLLINO_D7, OUTPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
+  pinMode(CONTROLLINO_D7, OUTPUT);
 
 }
 
 
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// Read analog values for time high and time low
-iTimeHigh = analogRead ( CONTROLLINO_A6 );
-iTimeLow  = analogRead ( CONTROLLINO_A7 );
+  // Lire les valeurs analogiques pour la durée haute et la durée basse
+  iDureeHaute = analogRead(CONTROLLINO_A6);
+  iDureeBasse = analogRead(CONTROLLINO_A7);
 
-if (digitalRead (CONTROLLINO_A0) )
-{
-  digitalWrite ( CONTROLLINO_D3, HIGH);
-  delay ( iTimeHigh*4 );                  // *4 - gives a more visible result
-  digitalWrite ( CONTROLLINO_D3, LOW);
-  delay ( iTimeLow*4 );                   // *4 - gives a more visible result
+  if (digitalRead(CONTROLLINO_A0))
+  {
+    digitalWrite(CONTROLLINO_D3, HIGH);
+    delay(iDureeHaute * 4);  // * 4 - donne un résultat plus visible
+    digitalWrite(CONTROLLINO_D3, LOW);
+    delay(iDureeBasse * 4);  // * 4 - donne un résultat plus visible
+  }
 }
-} //loop
