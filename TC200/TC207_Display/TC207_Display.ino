c8ff61ae4bfa3df9b91ea3fd3799a571177022ec
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:25:07 +0100

    Update TC207_Display.ino

--- TC200/TC207_Display/TC207_Display.ino
+++ TC200/TC207_Display/TC207_Display.ino
@@ -2,46 +2,43 @@
 #include <Controllino.h>
 
 //
-//  Task: TC201_Corridor_Light
+// Tâche : TC201_Lumière_du_couloir
 //
 
-//  definition of variables
-
+// Définition des variables
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A2, INPUT);
-pinMode(CONTROLLINO_A3, INPUT);
-pinMode(CONTROLLINO_A4, INPUT);
-pinMode(CONTROLLINO_A5, INPUT);
-pinMode(CONTROLLINO_A6, INPUT);
-pinMode(CONTROLLINO_A7, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A2, INPUT);
+  pinMode(CONTROLLINO_A3, INPUT);
+  pinMode(CONTROLLINO_A4, INPUT);
+  pinMode(CONTROLLINO_A5, INPUT);
+  pinMode(CONTROLLINO_A6, INPUT);
+  pinMode(CONTROLLINO_A7, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-pinMode(CONTROLLINO_D7, OUTPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
+  pinMode(CONTROLLINO_D7, OUTPUT);
 
 }
 
-
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// The Controllino MEGA Automation comes without an builtin Display
+  // Le Controllino MEGA Automation n'est pas équipé d'un écran intégré.
 
-// Therfore we will not provide any programm here. 
-
+  // Par conséquent, aucun programme particulier n'est fourni ici.
 
 } //loop
