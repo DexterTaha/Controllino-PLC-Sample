76f3d66a093ad756af38a78ebd4aac7ad5b58a38
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:31:09 +0100

    Update TC300_Car_park.ino

--- TC300/TC300_Car_park/TC300_Car_park.ino
+++ TC300/TC300_Car_park/TC300_Car_park.ino
@@ -2,79 +2,73 @@
 #include <Controllino.h>
 
 //
-//  Task: TC300_Car_park
+// Tâche : TC300_Parking
 //
 
-//  definition of variables
+// Définition des variables
 
-int   iCounter;       // count number of cars
-bool  bFlank=LOW;     // debounce light barrier 
-
+int iCompteur;      // compter le nombre de voitures
+bool bFlanc = LOW;  // rebondissement de la barrière lumineuse
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A2, INPUT);
-pinMode(CONTROLLINO_A7, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A2, INPUT);
+  pinMode(CONTROLLINO_A7, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
 }
 
-
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// Light barrier entrance, count car
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A0) == HIGH ) )  {
-  iCounter++;
-  bFlank = HIGH;         // count only while flak switchs from Low -> High
-}
-
-// Reset counter to 0
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A1) == HIGH ) )  {
-  iCounter = 0;
-  bFlank = HIGH;
-}
-
-// adjust counter +1
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A2) == HIGH ) )  {
-  iCounter++;
-  bFlank = HIGH;
-}
-
-// Light barrier exit
-if ( (bFlank == LOW )  && (digitalRead (CONTROLLINO_A7) == HIGH ) )  {
-  iCounter--;
-  if ( iCounter < 0 ) iCounter = 0;
-  bFlank = HIGH;
-}
-
-// Light barrier entrance
-if ( iCounter < 5  ) {
-  digitalWrite(CONTROLLINO_D0, HIGH);
-  digitalWrite(CONTROLLINO_D1, LOW);
-    }
-
-// Light barrier entrance
-if ( iCounter >= 5  ) {
-  digitalWrite(CONTROLLINO_D0, LOW);
-  digitalWrite(CONTROLLINO_D1, HIGH);
-    }
-
-
-// reset variable iFlanke only while flank switches from High -> Low
-if ( !(digitalRead (CONTROLLINO_A0)) && !(digitalRead (CONTROLLINO_A1)) && !(digitalRead (CONTROLLINO_A2)) && !(digitalRead (CONTROLLINO_A7)) )  {
-  bFlank = LOW;
-  delay (20);     //
+  // Barrière lumineuse d'entrée, compter la voiture
+  if ((bFlanc == LOW) && (digitalRead(CONTROLLINO_A0) == HIGH)) {
+    iCompteur++;
+    bFlanc = HIGH; // compter seulement lorsque le flanc passe de Low -> High
   }
 
- 
-} //loop
+  // Réinitialiser le compteur à 0
+  if ((bFlanc == LOW) && (digitalRead(CONTROLLINO_A1) == HIGH)) {
+    iCompteur = 0;
+    bFlanc = HIGH;
+  }
+
+  // Ajuster le compteur +1
+  if ((bFlanc == LOW) && (digitalRead(CONTROLLINO_A2) == HIGH)) {
+    iCompteur++;
+    bFlanc = HIGH;
+  }
+
+  // Barrière lumineuse de sortie
+  if ((bFlanc == LOW) && (digitalRead(CONTROLLINO_A7) == HIGH)) {
+    iCompteur--;
+    if (iCompteur < 0) iCompteur = 0;
+    bFlanc = HIGH;
+  }
+
+  // Barrière lumineuse d'entrée
+  if (iCompteur < 5) {
+    digitalWrite(CONTROLLINO_D0, HIGH);
+    digitalWrite(CONTROLLINO_D1, LOW);
+  }
+
+  // Barrière lumineuse d'entrée
+  if (iCompteur >= 5) {
+    digitalWrite(CONTROLLINO_D0, LOW);
+    digitalWrite(CONTROLLINO_D1, HIGH);
+  }
+
+  // Réinitialiser la variable bFlanc uniquement lorsque le flanc passe de High -> Low
+  if (!(digitalRead(CONTROLLINO_A0)) && !(digitalRead(CONTROLLINO_A1)) && !(digitalRead(CONTROLLINO_A2)) && !(digitalRead(CONTROLLINO_A7))) {
+    bFlanc = LOW;
+    delay(20); //
+  }
+}
