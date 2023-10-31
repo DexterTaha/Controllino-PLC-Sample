829bd954c5a4e6db7a699aef5d8aea02b9d2be38
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 10:47:11 +0100

    Update TC102_FlipFlop_SurgeRelay.ino

--- TC100/TC102_FlipFlop_SurgeRelay/TC102_FlipFlop_SurgeRelay.ino
+++ TC100/TC102_FlipFlop_SurgeRelay/TC102_FlipFlop_SurgeRelay.ino
@@ -2,68 +2,62 @@
 #include <Controllino.h>
 
 //
-//  Task: TC102_FlipFlop_SurgeRelay
+// Tâche : TC102_Bascule_RelayTempête
 //
 
-//  definition of variables
-
+// Définition des variables
 bool bRS1;
 bool bRS2;
-bool bToggleOnce;
+bool bBasculerUneFois;
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
-
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
-pinMode(CONTROLLINO_A3, INPUT);
-pinMode(CONTROLLINO_A4, INPUT);
-pinMode(CONTROLLINO_A5, INPUT);
-
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-
+  // Définir les broches en tant qu'entrées ou sorties
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A3, INPUT);
+  pinMode(CONTROLLINO_A4, INPUT);
+  pinMode(CONTROLLINO_A5, INPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
 }
 
 //
-//  Loop
+// Boucle
 //
 void loop() {
+  // Bascule
 
-// FlipFlop
+  // S R
+  // 0 0    x
+  // 0 1    0
+  // 1 0    1
+  // 1 1    0
 
-// S R
-// 0 0    x
-// 0 1    0
-// 1 0    1
-// 1 1    0
-
-if (digitalRead (CONTROLLINO_A0 ))
+  if (digitalRead(CONTROLLINO_A0))
     bRS1 = true;
-if (digitalRead (CONTROLLINO_A1 ))
+  if (digitalRead(CONTROLLINO_A1))
     bRS1 = false;
 
-digitalWrite (CONTROLLINO_D0, bRS1);
+  digitalWrite(CONTROLLINO_D0, bRS1);
 
-// SurgeRelay
+  // RelaisTempête
 
-if (digitalRead (CONTROLLINO_A3 ) && (bToggleOnce == false))
-    {
-      bRS2 = !bRS2;
-      bToggleOnce=true;
-      delay ( 50 ); 
-    }
-if ( !digitalRead (CONTROLLINO_A3 ) && (bToggleOnce == true))
-    {
-      bToggleOnce=false;
-      delay ( 50 ); 
-    }
-if (digitalRead (CONTROLLINO_A4 ))
+  if (digitalRead(CONTROLLINO_A3) && (bBasculerUneFois == false)) {
+    bRS2 = !bRS2;
+    bBasculerUneFois = true;
+    delay(50);
+  }
+  if (!digitalRead(CONTROLLINO_A3) && (bBasculerUneFois == true)) {
+    bBasculerUneFois = false;
+    delay(50);
+  }
+  if (digitalRead(CONTROLLINO_A4))
     bRS2 = true;
-if (digitalRead (CONTROLLINO_A5 ))
+  if (digitalRead(CONTROLLINO_A5))
     bRS2 = false;
 
-digitalWrite (CONTROLLINO_D1, bRS2);
-} 
+  digitalWrite(CONTROLLINO_D1, bRS2);
+}
