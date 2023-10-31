5da0e062711bd95dd79cda5e41d6dc616cadc6c0
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:32:12 +0100

    Update TC301_Running_light.ino

--- TC300/TC301_Running_light/TC301_Running_light.ino
+++ TC300/TC301_Running_light/TC301_Running_light.ino
@@ -2,115 +2,112 @@
 #include <Controllino.h>
 
 //
-//  Task: TC301_Running_light
+// Tâche : TC301_Lumière_clignotante
 //
 
-//  definition of variables
+// Définition des variables
 
-int   iCounter=0;    // counter  0 to 7
-bool  bOn;           // true-On, false-Off
-bool  bDirection;    // false-right, true-left
-
+int iCompteur = 0;  // compteur de 0 à 7
+bool bAllume;      // true-Allumé, false-Éteint
+bool bDirection;   // false-droite, true-gauche
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
 
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A1, INPUT);
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A1, INPUT);
 
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-pinMode(CONTROLLINO_D7, OUTPUT);
-
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
+  pinMode(CONTROLLINO_D7, OUTPUT);
 }
 
-
 //
-//  Loop
+// Boucle
 //
 void loop() {
 
-// Check if counter is over 7
-if (iCounter == 8)
-  iCounter=0;
+  // Vérifier si le compteur dépasse 7
+  if (iCompteur == 8)
+    iCompteur = 0;
 
-// Check if counter is under 0
-if (iCounter == -1)
-  iCounter=7;
+  // Vérifier si le compteur est inférieur à 0
+  if (iCompteur == -1)
+    iCompteur = 7;
 
-// counter on/off
-if ( digitalRead (CONTROLLINO_A0) == HIGH ) 
-    bOn = true;
+  // Compteur allumé/éteint
+  if (digitalRead(CONTROLLINO_A0) == HIGH)
+    bAllume = true;
   else
-    bOn = false;
+    bAllume = false;
 
-// counter directorn
-if ( digitalRead (CONTROLLINO_A1) == HIGH )
+  // Direction du compteur
+  if (digitalRead(CONTROLLINO_A1) == HIGH)
     bDirection = true;
   else
     bDirection = false;
 
-// iCounter = 0
-if ( bOn && (iCounter == 0) )
-    digitalWrite ( CONTROLLINO_D0, HIGH);
+  // iCompteur = 0
+  if (bAllume && (iCompteur == 0))
+    digitalWrite(CONTROLLINO_D0, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D0, LOW);
+    digitalWrite(CONTROLLINO_D0, LOW);
 
-// iCounter = 1
-if ( bOn && (iCounter == 1) )
-    digitalWrite ( CONTROLLINO_D1, HIGH);
+  // iCompteur = 1
+  if (bAllume && (iCompteur == 1))
+    digitalWrite(CONTROLLINO_D1, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D1, LOW);
+    digitalWrite(CONTROLLINO_D1, LOW);
 
-// iCounter = 2
-if ( bOn && (iCounter == 2) )
-    digitalWrite ( CONTROLLINO_D2, HIGH);
+  // iCompteur = 2
+  if (bAllume && (iCompteur == 2))
+    digitalWrite(CONTROLLINO_D2, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D2, LOW);
+    digitalWrite(CONTROLLINO_D2, LOW);
 
-// iCounter = 3
-if ( bOn && (iCounter == 3) )
-    digitalWrite ( CONTROLLINO_D3, HIGH);
+  // iCompteur = 3
+  if (bAllume && (iCompteur == 3))
+    digitalWrite(CONTROLLINO_D3, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D3, LOW);
+    digitalWrite(CONTROLLINO_D3, LOW);
 
-// iCounter = 4
-if ( bOn && (iCounter == 4) )
-    digitalWrite ( CONTROLLINO_D4, HIGH);
+  // iCompteur = 4
+  if (bAllume && (iCompteur == 4))
+    digitalWrite(CONTROLLINO_D4, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D4, LOW);
+    digitalWrite(CONTROLLINO_D4, LOW);
 
-// iCounter = 5
-if ( bOn && (iCounter == 5) )
-    digitalWrite ( CONTROLLINO_D5, HIGH);
+  // iCompteur = 5
+  if (bAllume && (iCompteur == 5))
+    digitalWrite(CONTROLLINO_D5, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D5, LOW);
+    digitalWrite(CONTROLLINO_D5, LOW);
 
-// iCounter = 6
-if ( bOn && (iCounter == 6) )
-    digitalWrite ( CONTROLLINO_D6, HIGH);
+  // iCompteur = 6
+  if (bAllume && (iCompteur == 6))
+    digitalWrite(CONTROLLINO_D6, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D6, LOW);
+    digitalWrite(CONTROLLINO_D6, LOW);
 
-// iCounter = 7
-if ( bOn && (iCounter == 7) )
-    digitalWrite ( CONTROLLINO_D7, HIGH);
+  // iCompteur = 7
+  if (bAllume && (iCompteur == 7))
+    digitalWrite(CONTROLLINO_D7, HIGH);
   else
-    digitalWrite ( CONTROLLINO_D7, LOW);
+    digitalWrite(CONTROLLINO_D7, LOW);
 
-delay (500);
+  delay(500);
 
-// Set new iCounter value
-if (bDirection == false )
-    iCounter++;
+  // Définir la nouvelle valeur de iCompteur
+  if (bDirection == false)
+    iCompteur++;
   else
-    iCounter--;
+    iCompteur--;
 
-} //loop
+}
