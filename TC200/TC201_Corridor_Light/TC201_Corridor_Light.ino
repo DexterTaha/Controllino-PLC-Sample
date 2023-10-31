a48b816c6a5ce1e05ed4e761380be28300c81b77
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2023-10-31 11:09:32 +0100

    Update TC201_Corridor_Light.ino

--- TC200/TC201_Corridor_Light/TC201_Corridor_Light.ino
+++ TC200/TC201_Corridor_Light/TC201_Corridor_Light.ino
@@ -2,70 +2,65 @@
 #include <Controllino.h>
 
 //
-//  Task: TC201_Corridor_Light
+// Tâche : TC201_Lumière_du_couloir
 //
 
-//  definition of variables
-
-const long iLightningTime = 5000;           // Lightning time of the normal lights
-const long iLightningTimeStandby = 2000;    // Lightning time of the standby light
-int iLight = LOW;                           // normal lights ON
-int iLightStandby = LOW;                    // standby light ON
-unsigned long previousMillis = 0;           // stored milliseconds
-unsigned long currentMillis;                // actual milliseconds
+// Définition des variables
+const long iDureeEclairage = 5000;         // Durée d'éclairage des lumières normales
+const long iDureeEclairageVeille = 2000;    // Durée d'éclairage de la lumière de veille
+int iLumiere = LOW;                         // Lumières normales allumées
+int iLumiereVeille = LOW;                   // Lumière de veille allumée
+unsigned long tempsPrecedent = 0;           // Millisecondes précédentes stockées
+unsigned long tempsActuel;                  // Millisecondes actuelles
 
 //
-//  Setup
+// Configuration
 //
 void setup() {
-
-pinMode(CONTROLLINO_A0, INPUT);
-pinMode(CONTROLLINO_A2, INPUT);
-pinMode(CONTROLLINO_A4, INPUT);
-pinMode(CONTROLLINO_A6, INPUT);
-
-pinMode(CONTROLLINO_D0, OUTPUT);
-pinMode(CONTROLLINO_D1, OUTPUT);
-pinMode(CONTROLLINO_D2, OUTPUT);
-pinMode(CONTROLLINO_D3, OUTPUT);
-pinMode(CONTROLLINO_D4, OUTPUT);
-pinMode(CONTROLLINO_D5, OUTPUT);
-pinMode(CONTROLLINO_D6, OUTPUT);
-
+  // Définir les broches en tant qu'entrées ou sorties
+  pinMode(CONTROLLINO_A0, INPUT);
+  pinMode(CONTROLLINO_A2, INPUT);
+  pinMode(CONTROLLINO_A4, INPUT);
+  pinMode(CONTROLLINO_A6, INPUT);
+  pinMode(CONTROLLINO_D0, OUTPUT);
+  pinMode(CONTROLLINO_D1, OUTPUT);
+  pinMode(CONTROLLINO_D2, OUTPUT);
+  pinMode(CONTROLLINO_D3, OUTPUT);
+  pinMode(CONTROLLINO_D4, OUTPUT);
+  pinMode(CONTROLLINO_D5, OUTPUT);
+  pinMode(CONTROLLINO_D6, OUTPUT);
 }
 
-
 //
-//  Loop
+// Boucle
 //
 void loop() {
+  // Connexion_logique
 
-// Logical_connection
+  tempsActuel = millis();
 
-currentMillis = millis();
-
-// switch off light after iLightningTime  
-if ((currentMillis - previousMillis >= iLightningTime) && (iLight == HIGH)) {
-    iLight = LOW;
-    iLightStandby = HIGH;
+  // Éteindre la lumière après iDureeEclairage
+  if ((tempsActuel - tempsPrecedent >= iDureeEclairage) && (iLumiere == HIGH)) {
+    iLumiere = LOW;
+    iLumiereVeille = HIGH;
     digitalWrite(CONTROLLINO_D0, LOW);
     digitalWrite(CONTROLLINO_D2, LOW);
     digitalWrite(CONTROLLINO_D3, LOW);
     digitalWrite(CONTROLLINO_D4, LOW);
     digitalWrite(CONTROLLINO_D6, LOW);
-}
+  }
 
-// switch off light after iLightningTimeStandby  
-if ((currentMillis - previousMillis >= iLightningTime + iLightningTimeStandby) && (iLightStandby == HIGH)) {
-    iLightStandby = LOW;
+  // Éteindre la lumière après iDureeEclairageVeille
+  if ((tempsActuel - tempsPrecedent >= iDureeEclairage + iDureeEclairageVeille) && (iLumiereVeille == HIGH)) {
+    iLumiereVeille = LOW;
     digitalWrite(CONTROLLINO_D1, LOW);
     digitalWrite(CONTROLLINO_D5, LOW);
-}
+  }
 
-// switch on lights 
-if ( digitalRead (CONTROLLINO_A0) || digitalRead (CONTROLLINO_A2) || digitalRead (CONTROLLINO_A4) || digitalRead (CONTROLLINO_A6)) {
-    iLight = HIGH;
-    iLightStandby = LOW;
+  // Allumer les lumières
+  if (digitalRead(CONTROLLINO_A0) || digitalRead(CONTROLLINO_A2) || digitalRead(CONTROLLINO_A4) || digitalRead(CONTROLLINO_A6)) {
+    iLumiere = HIGH;
+    iLumiereVeille = LOW;
     digitalWrite(CONTROLLINO_D0, HIGH);
     digitalWrite(CONTROLLINO_D1, HIGH);
     digitalWrite(CONTROLLINO_D2, HIGH);
@@ -73,7 +68,6 @@ if ( digitalRead (CONTROLLINO_A0) || digitalRead (CONTROLLINO_A2) || digitalRead
     digitalWrite(CONTROLLINO_D4, HIGH);
     digitalWrite(CONTROLLINO_D5, HIGH);
     digitalWrite(CONTROLLINO_D6, HIGH);
-    previousMillis = currentMillis;
+    tempsPrecedent = tempsActuel;
   }
-
-} //loop
+}
